9b32f78d77a9fe6697d5ac2c5eab7563cb1254e3
somya <somyaagarwal3745@gmail.com>
2023-08-19 21:27:21 +0530

    added

---
+++ react.js
@@ -0,0 +1,17 @@
+// function example() {
+//     var x = 10;
+//     if (true) {
+//         const y = 20;
+//     }
+//     console.log(x);
+//     console.log(y);
+// }
+// example();
+
+
+
+
+// ---------hosting
+console.log(x);
+let x = 10;     //const can be accessable  and let also can't be hostable var ki hosting karwa sakte h//
+console.log(x);
